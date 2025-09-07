f406e6fa6e2ca24ff1073920e02b35b602b1a1c9
Atheer97m <xatheer.525@gmail.com>
2025-09-07 04:13:28 +0300

    Complete SQL & Python 1 challenge

--- queries.py
+++ queries.py
@@ -1,30 +1,32 @@
 def number_of_directors(db):
-    query = """
-    SELECT COUNT(*) FROM directors
-    """
+    query = "SELECT COUNT(*) FROM directors;"
     db.execute(query)
     result = db.fetchone()
     return result[0]
 
 def list_of_directors(db):
-    query = """
-    SELECT name FROM directors
-    ORDER BY name ASC
-    """
+    query = "SELECT name FROM directors ORDER BY name ASC;"
     db.execute(query)
     results = db.fetchall()
     return [row[0] for row in results]
 
 def list_of_movies_about_river(db):
-    query = """
-    SELECT title FROM movies
-    WHERE
-      title LIKE '% river %' OR
-      title LIKE 'river %' OR
-      title LIKE '% river' OR
-      title = 'river'
-    ORDER BY title ASC
-    """
+    query = "SELECT title FROM movies WHERE title LIKE '%river%' ORDER BY title ASC;"
     db.execute(query)
     results = db.fetchall()
     return [row[0] for row in results]
+
+def number_of_directors_like(db, word):
+    query = "SELECT COUNT(*) FROM directors WHERE name LIKE ?;"
+    db.execute(query, [f"%{word}%"])
+    result = db.fetchone()
+    return result[0]
+
+def list_of_movies_longer_than(db, min_length):
+    query = "SELECT title FROM movies WHERE minutes > ? ORDER BY title ASC;"
+    db.execute(query, [min_length])
+    results = db.fetchall()
+    return [row[0] for row in results]
+
+
+
